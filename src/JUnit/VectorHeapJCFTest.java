fa63993483e24e697c96dd092b8e544e66c6e4b1
Nahomy Castro <ncastrom21@gmail.com>
2023-04-02 15:20:01 -0600

    Implementación de JUnit para VectorHeapJCF. Programa Finalizado.

--- src/JUnit/VectorHeapJCFTest.java
+++ src/JUnit/VectorHeapJCFTest.java
@@ -1,26 +1,68 @@
 package JUnit;
 
-import org.junit.jupiter.api.Test;
+import Model.ProcessL;
+import Model.VectorHeapJCF;
+
+import static org.junit.Assert.assertEquals;
 
 class VectorHeapJCFTest {
 
-    @Test
+    /**
+     * Add.
+     */
+    @org.junit.jupiter.api.Test
     void add() {
+        VectorHeapJCF vectorHeapJCF = new VectorHeapJCF<>();
+        ProcessL process1 = new ProcessL("ls", "xavier", "19");
+        vectorHeapJCF.add(process1);
+        ProcessL expectedResult = process1;
+        assertEquals(expectedResult, vectorHeapJCF.getFirst());
     }
 
-    @Test
+    /**
+     * Remove.
+     */
+    @org.junit.jupiter.api.Test
     void remove() {
+        VectorHeapJCF vectorHeapJCF = new VectorHeapJCF<>();
+        ProcessL process1 = new ProcessL("ls", "xavier", "19");
+        ProcessL process2 = new ProcessL("cd", "nahomy", "-20");
+        vectorHeapJCF.add(process1);
+        vectorHeapJCF.add(process2);
+        ProcessL expectedResult = process2;
+        assertEquals(expectedResult, vectorHeapJCF.remove());
     }
 
-    @Test
+    /**
+     * Gets first.
+     */
+    @org.junit.jupiter.api.Test
+    void getFirst() {
+        VectorHeapJCF vectorHeapJCF = new VectorHeapJCF<>();
+        ProcessL process1 = new ProcessL("ls", "xavier", "19");
+        vectorHeapJCF.add(process1);
+        ProcessL expectedResult = process1;
+        assertEquals(expectedResult, vectorHeapJCF.getFirst());
+    }
+
+    /**
+     * Is empty.
+     */
+    @org.junit.jupiter.api.Test
     void isEmpty() {
+        VectorHeapJCF vectorHeapJCF = new VectorHeapJCF<>();
+        boolean expectedResult = true;
+        assertEquals(expectedResult, vectorHeapJCF.isEmpty());
     }
 
-    @Test
+    /**
+     * Size.
+     */
+    @org.junit.jupiter.api.Test
     void size() {
-    }
+        VectorHeapJCF vectorHeapJCF = new VectorHeapJCF<>();
+        int expectedResult = 0;
+        assertEquals(expectedResult, vectorHeapJCF.size());
 
-    @Test
-    void clear() {
     }
 }
