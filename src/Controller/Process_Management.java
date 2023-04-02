62e03e113029521a7a3b7d8d13f08e7908562578
Nahomy Castro <ncastrom21@gmail.com>
2023-04-02 14:58:44 -0600

    Se añadió codifiación para el método pqUsingVHJCF y se añadieron implementaciones al userMenu.

--- src/Controller/Process_Management.java
+++ src/Controller/Process_Management.java
@@ -2,6 +2,7 @@ package Controller;
 
 import Model.ProcessL;
 import Model.VectorHeap;
+import Model.VectorHeapJCF;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -31,5 +32,25 @@ public class Process_Management {
 
     }
 
+    public void pqUsingVHJCF(ArrayList<String> line, Vector<ProcessL> processList, Scanner sc){
+
+        processList = new Vector<>();
+        boolean printQueue = true;
+
+        for (String processInfo: line){
+            String[] processes = processInfo.trim().split(",");
+            processList.add(new ProcessL(processes[0], processes[1], processes[2]));
+        }
+
+        VectorHeapJCF processQueue = new VectorHeapJCF(processList);
+
+        while(printQueue){
+            System.out.println(processQueue.remove());
+            System.out.println("Show next process? (Y/N)");
+            String continueText = sc.nextLine();
+            printQueue = continueText.equalsIgnoreCase("y") && !processQueue.isEmpty();
+        }
+
+    }
 
 }
--- src/UI/DriverProgram.java
+++ src/UI/DriverProgram.java
@@ -3,7 +3,6 @@ package UI;
 import Controller.Process_Management;
 import Controller.ReadFile;
 import Model.ProcessL;
-import Model.VectorHeap;
 
 import java.util.Scanner;
 import java.util.Vector;
@@ -25,8 +24,6 @@ public class DriverProgram {
         int selection = sc.nextInt();
 
         System.out.println("Welcome to Priority Queue Systems\nWe're reading your file, wait a minute...");
-        String filePath = "src/userProcess.txt";
-        System.out.println(rf.readUserTxt(filePath).size());
         System.out.println("Please, select the type of VectorHeap to use: \n\t1. VectorHeap\n2. VectorHeap from JCF\n3.Finish program");
 
         switch(selection){
@@ -38,6 +35,7 @@ public class DriverProgram {
 
             case 2:
                 System.out.println("Processes sorted by Linux priority using VectorHeap from JCF:");
+                processManagement.pqUsingVHJCF(rf.readUserTxt("src/userProcess.txt"), processList, sc);
                 break;
 
             case 3:
